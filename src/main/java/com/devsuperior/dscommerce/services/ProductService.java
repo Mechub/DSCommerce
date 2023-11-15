7a28da1c3fbb044d99c6a7ca1598eb931efd6f1b
Mercio franca <mercioalbuquerque@hotmail.com>
2023-11-15 01:01:32 -0300

    POST new product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dscommerce.services;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
-
 
 @Service
 public class ProductService {
@@ -30,4 +27,20 @@ public class ProductService {
     Page<Product> result = repository.findAll(pageable);
     return result.map(x -> new ProductDTO(x));
   }
+
+  @Transactional
+  public ProductDTO insert(ProductDTO dto){
+    
+    Product entity = new Product();
+    entity.setName(dto.getName());
+    entity.setDescription(dto.getDescription());
+    entity.setPrice(dto.getPrice());
+    entity.setImgUrl(dto.getImgUrl());
+
+    entity = repository.save(entity);
+
+    return new ProductDTO(entity);
+
+  }
+
 }
